69a06ee2314872c649393ec7805e6a11b42c2440
Javier Palacios <jp.palacios@outlook.com>
2018-05-21 01:00:46 -0400

    turretrotation

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Turret : MonoBehaviour {
     private float timeshot;
     public float outshot;
-    public GameObject proyectile;
+    public GameObject bullet;
+    private Transform player;
+    public Transform partToRotate;
+    public float turnSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         timeshot = outshot;
-	}
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //establecimiento de los tiempos de disparo, esto para determinar cuan seguido la torreta creara y lanzara disparos
-		if (timeshot <= 0)
+
+        Vector3 dir = (player.position - transform.position);
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Debug.Log(lookRotation);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, 0f, rotation.z);
+
+        if (timeshot <= 0)
         {
-            Instantiate(proyectile, transform.position, Quaternion.identity);
+            Instantiate(bullet, transform.position, Quaternion.identity);
             timeshot = outshot;
         }
         else
