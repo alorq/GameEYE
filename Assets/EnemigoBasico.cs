415dc62d848213a3efd825af34a112e5c4098224
Tomas Venegas <38511899+FlaminEon@users.noreply.github.com>
2018-06-05 14:38:51 -0400

    Creación de script de enemigo básico (Sin Terminar)

---
+++ Assets/EnemigoBasico.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoBasico : MonoBehaviour {
+    [SerializeField] private LayerMask enemigo;
+    [SerializeField] private float velocidad = 1;
+    private Rigidbody2D rb;
+    private Transform sprite;
+    private float ancho;
+
+	void Start () {
+        //inicializacion de elementos y busqueda del borde del sprite
+        sprite = this.sprite;
+        rb = this.GetComponent<Rigidbody2D>();
+        ancho = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+	}
+	
+	void FixedUpdate () {
+        //Revisa si en el borde del sprite hay suelo
+        Vector2 borde = sprite.position - sprite.right * ancho;
+        bool suelo = Physics2D.Linecast(borde, borde + Vector2.down, enemigo);
+
+        //Si deja de tocar suelo el sprite va a cambiar de direccion
+        if (!suelo){
+            Vector3 rotacion = sprite.eulerAngles;
+            rotacion.y += 180;
+            sprite.eulerAngles = rotacion;
+        }
+
+        //Movimiento constante
+        Vector2 movimiento = rb.velocity;
+        movimiento.x = velocidad;
+        rb.velocity = movimiento;
+	}
+}
