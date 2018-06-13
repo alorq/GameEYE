d80a7971d41b296de499904430e5a4e4d87e202d
Tomas Venegas <38511899+FlaminEon@users.noreply.github.com>
2018-06-12 20:19:00 -0400

    Script Patrulla

---
+++ Assets/Scripts/PatrullaEnemigo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrullaEnemigo : MonoBehaviour {
+    [SerializeField] public float velocidad;
+    [SerializeField] public Transform deteccion;
+    private bool movimientoDerecha = true;
+	
+	void Update () {
+        transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+        RaycastHit2D contacto = Physics2D.Raycast(deteccion.position, Vector2.down, 2f);
+        if (contacto.collider == false) {
+            if (movimientoDerecha == true){
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movimientoDerecha = false;
+            }
+            else{
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movimientoDerecha = true;
+            }
+        }
+	}
+}
