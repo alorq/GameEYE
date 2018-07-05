05d991e803dbf52eb83eb4cd2c15d7fbfba94065
FlaminEon <38511899+FlaminEon@users.noreply.github.com>
2018-07-04 22:44:04 -0400

    Enemigo techo

---
+++ Assets/Scripts/CeilingSpike.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingSpike : MonoBehaviour{
+    public Transform deteccion;
+    public Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        caida();
+	}
+
+    void caida (){
+        RaycastHit2D deteccionJugador = Physics2D.Raycast(deteccion.position, Vector2.down, 50f);
+        if (deteccionJugador.collider.gameObject.tag == "Jugador"){
+            rb.gravityScale = 3;
+        }
+    }
+}
