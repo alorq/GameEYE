cf417f239c441f7a20f3778892a8390ad4953644
FlaminEon <38511899+FlaminEon@users.noreply.github.com>
2018-07-23 21:47:25 -0400

    Pls revisen el CeilingSpike.cs que no funciona
    La cosa tiene que detectar que un objeto pasa por debajo para caer

--- Assets/Scripts/CeilingSpike.cs
+++ Assets/Scripts/CeilingSpike.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CeilingSpike : MonoBehaviour{
-    public Transform deteccion;
+    public Transform deteccionInicio, deteccionFin;
     public Rigidbody2D rb;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
-	
+	void Update () {
+        raycast();
+    }
 	void FixedUpdate () {
         caida();
 	}
 
     void caida (){
-        RaycastHit2D deteccionJugador = Physics2D.Raycast(deteccion.position, Vector2.down, 50f);
+        RaycastHit2D deteccionJugador = Physics2D.Raycast(deteccionInicio.position, -Vector2.up, 50f);
         if (deteccionJugador.collider.gameObject.tag == "Jugador"){
             rb.gravityScale = 3;
         }
+    }
+
+    void raycast()
+    {
+        Debug.DrawLine(deteccionInicio.position, deteccionFin.position, Color.green);
     }
 }
