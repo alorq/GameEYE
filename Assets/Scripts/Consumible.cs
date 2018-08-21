cd33d87dda1f0a1674333b5fdef4d211be30cc88
Alex Orquera <alex.orquera@alumnos.ucn.cl>
2018-08-21 01:13:56 -0300

    Scripts iniciales
    Script de impulso no esta funcional, posible conflicto entre velocidades de ControlJugador e Impulso

---
+++ Assets/Scripts/Consumible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumible : MonoBehaviour {
+    AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collider2D collision)
+    {
+        if(collision.transform.gameObject.name == "Jugador")
+        {
+            audio.Play();
+            Destroy(this.gameObject);
+        }
+    }
+}
