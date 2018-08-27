41c15981359cdce45fbdcf0ac1a1d5a39b334fbb
FlaminEon <38511899+FlaminEon@users.noreply.github.com>
2018-08-26 22:54:57 -0300

    Menú Principal
    Implementación final del menú principal

---
+++ Assets/Scripts/EfectoSonido.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectoSonido : MonoBehaviour
+{
+    public AudioSource Fx;
+    public AudioClip hover;
+
+    public void hoverSound()
+    {
+        Fx.PlayOneShot(hover);
+    }
+}
---
+++ Assets/Scripts/menuPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuPrincipal : MonoBehaviour
+{
+    private float tiempo = 1f;
+
+    public void Jugar()
+    {
+        StartCoroutine(jugarNivel());
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo de la aplicación");
+        StartCoroutine(salirAplicacion());
+    }
+
+    IEnumerator jugarNivel()
+    {
+        Debug.Log("Esperando SFX");
+        yield return new WaitForSeconds(tiempo);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Nivel Cargado");
+    }
+
+    IEnumerator salirAplicacion()
+    {
+        Debug.Log("Esperando SFX");
+        yield return new WaitForSeconds(tiempo);
+        Debug.Log("Aplicación Cerrada");
+        Application.Quit();
+    }
+}
