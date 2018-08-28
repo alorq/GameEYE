f1cfa24c660dd5510bb4a7579015e21f4efaf016
FlaminEon <38511899+FlaminEon@users.noreply.github.com>
2018-08-27 21:47:07 -0300

    Ajustes
    Ajustado el rendimiento del menú principal, agregadas confirmaciones de salir del nivel y salir del juego. Agregado menú de pausa. Agregada variable Static boolean juegoEnPausa para verificar que el juego se encuentre en pausa desde cualquier script (scriptPausa.juegoEnPausa true or false). Prefab de menú se pausa se encuentra en la carpeta niveles bajo el nombre de MenuPausa.prefab

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    private float tiempo = 1f;
+
+    public void Confirmar()
+    {
+        Debug.Log("Regresando al menú principal");
+        StartCoroutine(confirmarSalida());
+    }
+
+    IEnumerator confirmarSalida()
+    {
+        Debug.Log("Esperando SFX");
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(tiempo);
+        SceneManager.LoadScene(0);
+        Debug.Log("Nivel cerrado");
+        scriptPausa.juegoEnPausa = false;
+    }
+}
---
+++ Assets/Scripts/scriptPausa.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPausa : MonoBehaviour {
+
+    public static bool juegoEnPausa;
+    public GameObject IUPausa;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoEnPausa)
+            {
+                Continuar();
+            }
+            else
+            {
+                Detener();
+            }
+
+        }
+	}
+
+    public void Continuar()
+    {
+        IUPausa.SetActive(false);
+        Time.timeScale = 1f;
+        juegoEnPausa = false;
+    }
+
+    void Detener()
+    {
+        IUPausa.SetActive(true);
+        Time.timeScale = 0f;
+        juegoEnPausa = true;
+    }
+}
