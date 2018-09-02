bf630b0dd8aad83f49cfa61edd9d469e5bb23362
Javier Palacios <jp.palacios@outlook.com>
2018-09-01 21:36:22 -0300

    JugadorDisparo

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag != "Jugador")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -8,6 +8,12 @@ public class ControlJugador : MonoBehaviour {
     [SerializeField] public float fuerzasalto;
     [SerializeField] public bool terreno;
     [SerializeField] public float max;
+    [SerializeField] private Transform canonr;
+    [SerializeField] private Transform canonl;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] private float velocidadbala;
+    [SerializeField] int totalAmountOfBullets;
+    [SerializeField] List<GameObject> bullets = new List<GameObject>();
     private Rigidbody2D rbd;
     private Animator ani;
     private bool disparadorsalto;
@@ -20,6 +26,13 @@ public class ControlJugador : MonoBehaviour {
         rbd = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
         vidaActual = vida;
+        for (int i = 0; i < totalAmountOfBullets; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, new Vector3(-1000, -1000, -1000), Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+            bullets.Add(bullet);
+        }
     }
 
     void Update(){
@@ -31,7 +44,36 @@ public class ControlJugador : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        SpriteRenderer ren = GetComponent<SpriteRenderer>();
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject bullet = bullets[0];
+            bullets.RemoveAt(0);
+            Debug.Log(bullet.GetInstanceID());
+            bullet.SetActive(false);
+            if (ren.flipX == false)
+            {
+                bullet.transform.position = canonr.position;
+                bullet.transform.rotation = canonr.rotation;
+            }
+            if (ren.flipX == true)
+            {
+                bullet.transform.position = canonl.position;
+                bullet.transform.rotation = canonl.rotation;
+            }
+            bullet.SetActive(true);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.isKinematic = false;
+            if (ren.flipX == false){
+                rb.velocity = new Vector2(velocidadbala, Random.Range(-0.2f, 0.2f));
+            }
+            if (ren.flipX == true)
+            {
+                rb.velocity = new Vector2(-velocidadbala, Random.Range(-0.2f, 0.2f));
+            }
 
+
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) && rbd.velocity.y == 0)
         {
             disparadorsalto = true;
@@ -52,11 +94,17 @@ public class ControlJugador : MonoBehaviour {
         }
         if (h < 0)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            if (ren.flipX == false)
+            {
+                ren.flipX = true;
+            }
         }
         if (h > 0)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            if (ren.flipX == true)
+            {
+                ren.flipX = false;
+            }
         }
         if (vidaActual <= 0) {
             vivo = false;
@@ -120,6 +168,16 @@ public class ControlJugador : MonoBehaviour {
         if (col.gameObject.tag == "Bala"){
             vidaActual -= 45;
         }
+    }
+
+    IEnumerator ReUseBullet(GameObject bullet)
+    {
+        yield return new WaitForSeconds(3f);
+        bullet.transform.position = new Vector3(-1000, -1000, -1000);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+        bullets.Add(bullet);
     }
 
 }
