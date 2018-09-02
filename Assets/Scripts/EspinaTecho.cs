39431eb06ae0cdd3a4012b05ad716112b86fa2d1
Alex Orquera <alex.orquera@alumnos.ucn.cl>
2018-09-02 11:12:06 -0300

    Espina del techo

---
+++ Assets/Scripts/EspinaTecho.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspinaTecho : MonoBehaviour{
+    public GameObject origenDeteccion;
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+	}
+
+    void FixedUpdate () {
+        caidaEspina();
+	}
+
+    void caidaEspina(){
+        RaycastHit2D deteccionJugador = Physics2D.CircleCast(origenDeteccion.transform.position, 10f, Vector2.down, 10f);
+        if (deteccionJugador.collider.gameObject.tag == "Jugador")
+        {
+            rb.gravityScale = 3;
+            Debug.Log("Lol");
+        }
+    }
+}
