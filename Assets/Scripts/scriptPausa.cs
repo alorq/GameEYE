7210f45015fe4e5d0974374c190a876d9ab104ae
Javier Palacios <jp.palacios@outlook.com>
2018-09-03 17:08:06 -0300

    correcionmuerte

--- Assets/Scripts/scriptPausa.cs
+++ Assets/Scripts/scriptPausa.cs
@@ -23,6 +23,11 @@ public class scriptPausa : MonoBehaviour {
 
         }
 
+        if (playerIsDed)
+        {
+            StartCoroutine(GameOver());
+        }
+
     }
 
     public void Continuar()
