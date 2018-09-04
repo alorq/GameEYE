0b88350d3be8d005f333c92a834727f809417d55
Javier Palacios <jp.palacios@outlook.com>
2018-09-03 21:19:05 -0300

    camara

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -6,26 +6,14 @@ using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
-    [SerializeField] public float altura;
     public Transform Target;
     public float Velocidad = 0.125f;
     public Vector3 offset;
-   
 
     void FixedUpdate()
     {
-        if (Target.position.y > altura)
-        {
-            Vector3 posicion = Target.position + offset;
-            Vector3 suavizado = Vector3.Lerp(new Vector3(Target.position.x, altura, Target.position.z), posicion, Velocidad));
-            transform.position = suavizado;
-        }
-        else
-        {
-            Vector3 posicion = Target.position + offset;
-            Vector3 suavizado = Vector3.Lerp(transform.position, posicion, Velocidad);
-            transform.position = suavizado;
-        }
-        
+        Vector3 posicion = Target.position + offset;
+        Vector3 suavizado = Vector3.Lerp(transform.position, posicion, Velocidad);
+        transform.position = suavizado;
     }
 }
